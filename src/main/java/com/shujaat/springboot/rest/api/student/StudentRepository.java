516ae167d0c2263873ef430dd2cebb2d90b5199f
Mohd Shujaat Ali <32382909+shujaat786@users.noreply.github.com>
2019-05-01 14:05:00 +0530

    Global exception handling in Spring data REST

---
+++ src/main/java/com/shujaat/springboot/rest/api/student/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.shujaat.springboot.rest.api.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
